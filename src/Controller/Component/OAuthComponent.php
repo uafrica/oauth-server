a1a9dd9f8fa6dbf5b2396df8d39a0f6d7a9d2b4f
Kevin Chow <kevin@chow.red>
2017-09-25 10:03:39 +0900

    Remove old OAuthComponent unused code

--- src/Controller/Component/OAuthComponent.php
+++ src/Controller/Component/OAuthComponent.php
@@ -119,7 +119,6 @@ class OAuthComponent extends Component
             new \DateInterval('PT1H') // access tokens will expire after 1 hour
         );
 
-
         return $server;
     }
 
@@ -149,57 +148,6 @@ class OAuthComponent extends Component
     {
         $server = $this->_getAuthorizationServer($config);
         $resourceServer = $this->_getResourceServer($config);
-        // debug($server);exit;
-        // $server->setSessionStorage($this->_getStorage('session'));
-        // $server->setAccessTokenStorage($this->_getStorage('accessToken'));
-        // $server->setClientStorage($this->_getStorage('client'));
-        // $server->setScopeStorage($this->_getStorage('scope'));
-        // $server->setAuthCodeStorage($this->_getStorage('authCode'));
-        // $server->setRefreshTokenStorage($this->_getStorage('refreshToken'));
-
-        // $supportedGrants = isset($config['supportedGrants']) ? $config['supportedGrants'] : $this->config('supportedGrants');
-        // $supportedGrants = $this->_registry->normalizeArray($supportedGrants);
-        //
-        // foreach ($supportedGrants as $properties) {
-        //     $grant = $properties['class'];
-        //
-        //     if (!in_array($grant, $this->_allowedGrants)) {
-        //         throw new NotImplementedException(__('The {0} grant type is not supported by the OAuthServer'));
-        //     }
-        //
-        //     $className = '\\League\\OAuth2\\Server\\Grant\\' . $grant . 'Grant';
-        //     $objGrant = new $className();
-        //
-        //     if ($grant === 'Password') {
-        //         $objGrant->setVerifyCredentialsCallback(function ($username, $password) {
-        //             $controller = $this->_registry->getController();
-        //             $controller->Auth->constructAuthenticate();
-        //             $userfield = $controller->Auth->_config['authenticate']['Form']['fields']['username'];
-        //             $controller->request->data[$userfield] = $username;
-        //             $controller->request->data['password'] = $password;
-        //             $loginOk = $controller->Auth->identify();
-        //             if ($loginOk) {
-        //                 return $loginOk['id'];
-        //             } else {
-        //                 return false;
-        //             }
-        //         });
-        //     }
-        //
-        //     foreach ($properties['config'] as $key => $value) {
-        //         $method = 'set' . Inflector::camelize($key);
-        //         if (is_callable([$objGrant, $method])) {
-        //             $objGrant->$method($value);
-        //         }
-        //     }
-        //
-        //     $server->addGrantType($objGrant);
-        // }
-        //
-        // if ($this->config('accessTokenTTL')) {
-        //     $server->setAccessTokenTTL($this->config('accessTokenTTL'));
-        // }
-
         $this->Server = $server;
         $this->ResourceServer = $resourceServer;
     }
