fd94ba7be574948bdc62bc08ae2b7d474e0ee70f
nojimage <nojimage@gmail.com>
2020-01-14 17:29:49 +0900

    add ScopeRepository

---
+++ src/Model/Bridge/Repository/ScopeRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OAuthServer\Model\Bridge\Repository;
+
+use Cake\Datasource\ModelAwareTrait;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+use OAuthServer\Model\Table\OauthScopesTable;
+
+class ScopeRepository implements ScopeRepositoryInterface, EventDispatcherInterface
+{
+    use EventDispatcherTrait;
+    use ModelAwareTrait;
+
+    /**
+     * @var OauthScopesTable
+     */
+    private $table;
+
+    /**
+     * RefreshTokenRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->table = $this->loadModel('OAuthServer.OauthScopes');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getScopeEntityByIdentifier($identifier)
+    {
+        return $this->table->find()->where([$this->table->getPrimaryKey() => $identifier])->first();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function finalizeScopes(array $scopes, $grantType, ClientEntityInterface $clientEntity, $userIdentifier = null)
+    {
+        $result = $this->dispatchEvent('OAuthServer.finalizeScopes', compact(
+            'scopes',
+            'grantType',
+            'clientEntity',
+            'userIdentifier'
+        ));
+
+        if ($result->getResult()) {
+            $scopes = $result->getResult();
+        }
+
+        return (array)$scopes;
+    }
+}
