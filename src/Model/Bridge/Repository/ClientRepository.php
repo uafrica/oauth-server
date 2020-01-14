487b8f4f42aa370ee2fe5ae779035fd451370593
nojimage <nojimage@gmail.com>
2020-01-14 16:59:26 +0900

    add ClientRepository

---
+++ src/Model/Bridge/Repository/ClientRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OAuthServer\Model\Bridge\Repository;
+
+use Cake\Datasource\ModelAwareTrait;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use OAuthServer\Model\Table\OauthClientsTable;
+
+/**
+ * implemented ClientRepositoryInterface
+ */
+class ClientRepository implements ClientRepositoryInterface
+{
+    use ModelAwareTrait;
+
+    /**
+     * @var OauthClientsTable
+     */
+    private $table;
+
+    /**
+     * RefreshTokenRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->table = $this->loadModel('OAuthServer.OauthClients');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getClientEntity($clientIdentifier)
+    {
+        return $this->table->find()->where([$this->table->getPrimaryKey() => $clientIdentifier])->first();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validateClient($clientIdentifier, $clientSecret, $grantType)
+    {
+        $conditions = [
+            $this->table->getPrimaryKey() => $clientIdentifier,
+        ];
+        if ($clientSecret !== null) {
+            $conditions[$this->table->aliasField('client_secret')] = $clientSecret;
+        }
+
+        // TODO: process $grantType
+        // if ($grantType !== null) {
+        // }
+
+        return $this->table->exists($conditions);
+    }
+}
---
+++ tests/TestCase/Model/Bridge/Repository/ClientRepositoryTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace OAuthServer\Test\TestCase\Model\Bridge\Repository;
+
+use Cake\TestSuite\TestCase;
+use OAuthServer\Model\Bridge\Repository\ClientRepository;
+
+class ClientRepositoryTest extends TestCase
+{
+    public $fixtures = [
+        'plugin.OAuthServer.Clients',
+    ];
+
+    /**
+     * @var ClientRepository
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->repository = new ClientRepository();
+    }
+
+    public function tearDown()
+    {
+        unset($this->repository);
+        parent::tearDown();
+    }
+
+    public function testGetClientEntity()
+    {
+        $entity = $this->repository->getClientEntity('TEST');
+
+        $this->assertSame('TEST', $entity->getIdentifier());
+    }
+
+    /**
+     * @dataProvider dataValidateClient
+     * @param array $inputs
+     * @param bool $expects
+     */
+    public function testValidateClient($inputs, $expects)
+    {
+        [$clientIdentifier, $clientSecret, $grantType] = $inputs;
+
+        $this->assertSame($expects, $this->repository->validateClient($clientIdentifier, $clientSecret, $grantType));
+    }
+
+    public function dataValidateClient()
+    {
+        return [
+            'valid: Client id only' => [
+                ['TEST', null, null],
+                true,
+            ],
+            'valid: Client id with secret' => [
+                ['TEST', 'TestSecret', null],
+                true,
+            ],
+            'invalid: Client id only' => [
+                ['INVALID', null, null],
+                false,
+            ],
+            'invalid: Client id with secret' => [
+                ['TEST', 'invalid', null],
+                false,
+            ],
+        ];
+    }
+}
