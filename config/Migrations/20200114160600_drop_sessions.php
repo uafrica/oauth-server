5ab33c7e99382582b6089da87a9b3275083ef082
nojimage <nojimage@gmail.com>
2020-01-16 10:46:31 +0900

    add drop sessions

---
+++ config/Migrations/20200114160600_drop_sessions.php
@@ -0,0 +1,68 @@
+<?php
+/** @noinspection AutoloadingIssuesInspection */
+
+use Migrations\AbstractMigration;
+
+class DropSessions extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('oauth_scopes')->drop();
+        $this->table('oauth_sessions')->drop();
+    }
+
+    public function down()
+    {
+        $table = $this->table('oauth_sessions');
+        $table
+            ->addColumn(
+                'owner_model',
+                'string',
+                [
+                    'limit' => 200,
+                ]
+            )
+            ->addColumn(
+                'owner_id',
+                'string',
+                [
+                    'limit' => 20,
+                ]
+            )
+            ->addColumn(
+                'client_id',
+                'string',
+                [
+                    'limit' => 20,
+                ]
+            )
+            ->addColumn(
+                'client_redirect_uri',
+                'string',
+                [
+                    'default' => null,
+                    'limit' => 200,
+                    'null' => true,
+                ]
+            );
+        $table->create();
+
+        $table = $this->table('oauth_session_scopes');
+        $table
+            ->addColumn(
+                'session_id',
+                'integer',
+                [
+                    'length' => 11,
+                ]
+            )
+            ->addColumn(
+                'scope_id',
+                'string',
+                [
+                    'length' => 40,
+                ]
+            );
+        $table->create();
+    }
+}
